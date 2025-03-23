d0f154bf956a5f0362bfe61498a9d3b71c4a56bf
Dhiraj <dkskp2005@gmail.com>
2025-03-23 15:35:31 +0530

    initial main entry

---
+++ app/src/main/java/DRCTC/App.java
@@ -0,0 +1,13 @@
+package DRCTC;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class App {
+
+    public static void main(String[] args) {
+
+    }
+}
