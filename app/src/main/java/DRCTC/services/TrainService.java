99e54da767e94eba0976df9d8ee8466373c27a8a
Dhiraj <dkskp2005@gmail.com>
2025-03-26 22:18:09 +0530

    functions implement for train search

--- app/src/main/java/DRCTC/services/TrainService.java
+++ app/src/main/java/DRCTC/services/TrainService.java
@@ -1,20 +1,40 @@
 package DRCTC.services;
 
 import DRCTC.entities.Train;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class TrainService {
 
-    private List<Train> trainList;
+    private final List<Train> trainList;
+    private static final String TRAIN_DB_PATH = "../localDB/trains.json";
 
 
+    // default constructor
     public TrainService() throws IOException{
-        System.out.println("Train service");
+        File trains = new File(TRAIN_DB_PATH);
+        ObjectMapper objectMapper = new ObjectMapper();
+        trainList = objectMapper.readValue(trains, new TypeReference<List<Train>>() {});
     }
 
+    // search trains
     public List<Train> searchTrains(String source, String destination){
-        return trainList;
+        return trainList.stream().filter(train -> validTrain(train,source,destination)).toList();
+    }
+
+
+    // validate trains
+    private boolean validTrain(Train train, String source, String destination){
+        List<String> stationOrder = train.getStations();
+
+        int sourceIndex = stationOrder.indexOf(source.toLowerCase());
+        int destinationIndex = stationOrder.indexOf(destination.toLowerCase());
+
+        return sourceIndex != -1 && destinationIndex != -1 && sourceIndex < destinationIndex;
     }
 }
