368dc66bcfcd54cff9907e548d231f345e11f743
Dhiraj <dkskp2005@gmail.com>
2025-03-27 12:10:30 +0530

    helper functions

--- app/src/main/java/DRCTC/services/TrainService.java
+++ app/src/main/java/DRCTC/services/TrainService.java
@@ -7,7 +7,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
+import java.util.OptionalInt;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class TrainService {
 
@@ -27,6 +30,47 @@ public class TrainService {
         return trainList.stream().filter(train -> validTrain(train,source,destination)).toList();
     }
 
+    // add trains
+    public void addTrain(Train newTrain){
+        // check if a train with same trainId already exists
+        Optional<Train> existingTrain =
+                trainList.stream()
+                        .filter(train1 -> train1.getTrainId().
+                                equalsIgnoreCase(newTrain.getTrainId())).findFirst();
+
+        if(existingTrain.isPresent()){
+            updateTrain(newTrain);
+        }else{
+            trainList.add(newTrain);
+            saveTrainListToFile();
+        }
+    }
+
+    // updateTrain
+    public void updateTrain(Train updatedTrain){
+        // find the index of the train with the same trainId
+        OptionalInt index = IntStream.range(0, trainList.size())
+                .filter(i -> trainList.get(i).getTrainId().equalsIgnoreCase(updatedTrain.getTrainId()))
+                .findFirst();
+
+        if(index.isPresent()){
+            // if found, replace the exsisting train with updated one
+            trainList.set(index.getAsInt(), updatedTrain);
+            saveTrainListToFile();
+        }else{
+            addTrain(updatedTrain);
+        }
+    }
+
+    // save tainlist
+    private void saveTrainListToFile() {
+        try{
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.writeValue(new File(TRAIN_DB_PATH), trainList);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 
     // validate trains
     private boolean validTrain(Train train, String source, String destination){
