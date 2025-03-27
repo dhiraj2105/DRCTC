b2cde6b45659623bfae2cabeabbaa5d9a01ba680
Dhiraj <dkskp2005@gmail.com>
2025-03-27 12:12:44 +0530

    updated

--- app/src/main/java/DRCTC/services/UserBookingSerivce.java
+++ app/src/main/java/DRCTC/services/UserBookingSerivce.java
@@ -12,8 +12,7 @@ import DRCTC.util.UserServiceUtil;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.core.type.TypeReference;
 
-public class UserBookingSerivce
-{
+public class UserBookingSerivce {
     // We are creating a global user here ( user is an entity ) so that we can store
     // the user state for e.g. Login state , to use User globally
     private User user;
@@ -25,27 +24,28 @@ public class UserBookingSerivce
     private static final String USERS_PATH = "../localdb/users.json";
 
     // user constructor
-    public UserBookingSerivce(User user) throws IOException
-    {
+    public UserBookingSerivce(User user) throws IOException {
         this.user = user;
         loadUser();
     }
 
-    // default constructor of this class that loads user from file and load to userList
-    public  UserBookingSerivce() throws  IOException{
+    // default constructor of this class that loads user from file and load to
+    // userList
+    public UserBookingSerivce() throws IOException {
         loadUser();
     }
 
-    public List<User> loadUser() throws IOException{
+    public List<User> loadUser() throws IOException {
         File users = new File(USERS_PATH);
         ObjectMapper objectMapper = new ObjectMapper();
-        return objectMapper.readValue(users, new TypeReference<List<User>>() {});
+        return objectMapper.readValue(users, new TypeReference<List<User>>() {
+        });
     }
 
     // Login User
-    public  Boolean loginUser(){
+    public Boolean loginUser() {
 
-        Optional<User> foundUser = userList.stream().filter(user1 ->{
+        Optional<User> foundUser = userList.stream().filter(user1 -> {
             return user1.getName().equals(user.getName()) &&
                     UserServiceUtil.checkPassword(user.getPassword(), user1.getHashedPassword());
         }).findFirst();
@@ -54,19 +54,19 @@ public class UserBookingSerivce
     }
 
     // Signup user
-    public Boolean signUp(User user1){
-        try{
+    public Boolean signUp(User user1) {
+        try {
             userList.add(user1);
             saveUserListToFile();
             return Boolean.TRUE;
-        }catch (IOException ex){
+        } catch (IOException ex) {
             return Boolean.FALSE;
         }
     }
 
     // function to save user list to a file
     // we are serializing user here
-    private void saveUserListToFile() throws IOException{
+    private void saveUserListToFile() throws IOException {
         File usersFile = new File(USERS_PATH);
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.writeValue(usersFile, userList);
@@ -76,42 +76,45 @@ public class UserBookingSerivce
     // object to json => serialize
 
     // fetch ticket bookings of user
-    public  void fetchBooking(){
+    public void fetchBooking() {
         user.printTickets();
     }
 
     // cancel booking
-    public  Boolean cancelBooking(String ticketId){
+    public Boolean cancelBooking(String ticketId) {
         return Boolean.FALSE;
     }
 
     // get trains
-    public List<Train> getTrains(String source, String destination){
-        try{
+    public List<Train> getTrains(String source, String destination) {
+        try {
             TrainService trainService = new TrainService();
             return trainService.searchTrains(source, destination);
-        }catch (IOException e){
+        } catch (IOException e) {
             return new ArrayList<>();
         }
     }
 
-    public List<List<Integer>> fetchSeats(Train train){
+    public List<List<Integer>> fetchSeats(Train train) {
         return train.getSeats();
     }
-    public Boolean bookTrainSeat(Train train,int row, int seat){
-        try{
-         TrainService trainService = new TrainService();
-         List<List<Integer>> seats = train.getSeats();
-         if(row >= 0 && row < seats.size() && seat >= 0 && seat < seats.get(row).size()){
-             if(seats.get(row).get(seat) == 0){
-                 seats.get(row).set(seat, 1);
-                 train.setSeats(seats);
-                 trainService.addTrain(train);
-             }else{return  false;}
-         }else{
-             return false;
-         }
-        }catch (IOException e){
+
+    public Boolean bookTrainSeat(Train train, int row, int seat) {
+        try {
+            TrainService trainService = new TrainService();
+            List<List<Integer>> seats = train.getSeats();
+            if (row >= 0 && row < seats.size() && seat >= 0 && seat < seats.get(row).size()) {
+                if (seats.get(row).get(seat) == 0) {
+                    seats.get(row).set(seat, 1);
+                    train.setSeats(seats);
+                    trainService.addTrain(train);
+                } else {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        } catch (IOException e) {
             return Boolean.FALSE;
         }
     }
